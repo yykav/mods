0f9d8a826307ea10c5c8ab31736756dd717257c0
Haitham <hmmelsm@gmail.com>
2026-03-02 20:40:26 +0200

    types(mods): wire repr and template module members

--- types/mods.lua
+++ types/mods.lua
@@ -1,20 +1,24 @@
 ---@meta mods
 
+local repr ---@module "mods.repr"
+local template ---@module "mods.template"
+
 ---Entry point that exposes all modules under one table.
 ---@class mods
 ---@field is mods.is
 ---@field keyword mods.keyword
 ---@field List mods.List
 ---@field operator mods.operator
----@field repr mods.repr
 ---@field runtime mods.runtime
 ---@field Set mods.Set
 ---@field str mods.str
 ---@field stringcase mods.stringcase
 ---@field tbl mods.tbl
----@field template mods.template
 ---@field utils mods.utils
 ---@field validate mods.validate
-local M = {}
+local M = {
+  repr = repr,
+  template = template,
+}
 
 return M
