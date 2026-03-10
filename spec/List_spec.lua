1b3cc5e55973cfcfbeba9758edd3fd951f0ccf7b
Haitham <hmmelsm@gmail.com>
2026-03-10 09:16:40 +0200

    test(List): expand spec coverage

--- spec/List_spec.lua
+++ spec/List_spec.lua
@@ -1,24 +1,27 @@
 ---@diagnostic disable: undefined-global
 
-local mods = require "mods"
-
-local List = mods.List
-local Set = mods.Set
-local deepcopy = mods.tbl.deepcopy
+local List = require "mods.List"
+local Set = require "mods.Set"
+local tbl = require "mods.tbl"
 
 local fmt = string.format
 local upper = string.upper
 local len = string.len
 
 describe("mods.List", function()
-  local patterns = [[ _____ ____e ___de __c__ _b_d_ _bc__ _bcd_ a___e
-      a_c__ a_c_e ab___ abc__ abcd_ abcde edcba
-      ABC__ AZ___ _ZAZA AZAZA ]]
+  local deepcopy = tbl.deepcopy
+  local patterns = [[
+    _____ ____e ___de __c__ _b_d_ _bc__ _bcd_ a___e
+    a_c__ a_c_e ab___ abc__ abcd_ abcde edcba ABC__
+    AZ___ _ZAZA AZAZA
+    __2__ _12__ _1_3_ _123_ _1_3_ _12_4
+    a1 b2 abcabc  
+  ]]
 
   patterns:gsub("%S+", function(p)
     local ls = {}
     p:gsub(".", function(c)
-      ls[#ls + 1] = c ~= "_" and c or nil
+      ls[#ls + 1] = c ~= "_" and (tonumber(c) or c) or nil
     end)
     _G[p] = ls
   end)
@@ -31,11 +34,6 @@ describe("mods.List", function()
   local add     = function(a, b) return a + b end
   local ac, e   = a_c__, ____e
   local a1_e2   = { a = 1, e = 2 }
-  local a1      = { "a", 1 }
-  local b2      = { "b", 2 }
-  local n12     = { 1, 2 }
-  local n123    = { 1, 2, 3 }
-  local abcabc  = { "a", "b", "c", "a", "b", "c" }
   local words   = { "aa", "b", "ccc", "dd" }
   local by_len  = { List({ "b" }), List({ "aa", "dd" }), List({ "ccc" }) }
   local mixed   = { "a", 1, true, false, "b", "a", 2 }
@@ -45,66 +43,91 @@ describe("mods.List", function()
     ["boolean"] = List({ true, false}),
   }
 
+  it("equals() compares lists with shallow equality", function()
+    assert.is_true(List({ "a", "b" }):equals(List({ "a", "b" })))
+    assert.is_true(List({ "a", "b", a1 }):equals(List({ "a", "b", a1 })))
+    assert.is_false(List({ "a", "b", {} }):equals(List({ "a", "b", {} })))
+    assert.is_false(List({ "a", "b" }):equals(List({ "a", "c" })))
+    assert.is_false(List({ "a", "b" }):equals(List({ "a", "b", "c" })))
+
+    local a = {}
+    assert.is_true(List({ a }):equals(List({ a })))
+    assert.is_false(List({ {} }):equals(List({ {} })))
+  end)
+
+
   local tests = {
-    ------fname------|--list--|---params---|----expected----|-same_ref?---
-    { "all"          , _____  , { is_b   } , true           ,       },
-    { "all"          , abc__  , { is_b   } , false          ,       },
-    { "all"          , abc__  , { not_z  } , true           ,       },
-    { "any"          , abc__  , { is_b   } , true           ,       },
-    { "append"       , abc__  , {        } , abc__          , true  },
-    { "append"       , abc__  , { "d"    } , abcd_          , true  },
-    { "clear"        , abcde  , {        } , _____          , true  },
-    { "concat"       , abcde  , {        } , "abcde"        ,       },
-    { "contains"     , _bcd_  , {        } , false          ,       },
-    { "contains"     , _bcd_  , { "C"    } , false          ,       },
-    { "contains"     , _bcd_  , { "c"    } , true           ,       },
-    { "copy"         , abcde  , {        } , abcde          , false },
-    { "count"        , AZAZA  , {        } , 0              ,       },
-    { "count"        , AZAZA  , { 'A'    } , 3              ,       },
-    { "difference"   , a_c_e  , { _bcd_  } , a___e          , false },
-    { "drop"         , abc__  , { 1      } , _bc__          , false },
-    { "extend"       , abc__  , { ___de  } , abcde          , true  },
-    { "extract"      , abc__  , { not_c  } , ab___          , false },
-    { "filter"       , _bcd_  , { not_c  } , _b_d_          , false },
-    { "first"        , abc__  , {        } , "a"            ,       },
-    { "flatten"      , {ac,e} , {        } , a_c_e          , false },
-    { "group_by"     , mixed  , { type   } , by_type        , false },
-    { "group_by"     , words  , { len    } , by_len         , false },
-    { "index_if"     , abcde  , { is_c   } , 3              ,       },
-    { "index"        , abcde  , {        } , nil            ,       },
-    { "index"        , abcde  , { "c"    } , 3              ,       },
-    { "insert"       , a_c__  , { 2, "b" } , abc__          , true  },
-    { "insert"       , abc__  , { "d"    } , abcd_          , true  },
-    { "intersection" , a_c_e  , { _bcd_  } , __c__          , false },
-    { "invert"       , a___e  , {        } , a1_e2          , false },
-    { "join"         , abc__  , { ","    } , "a,b,c"        ,       },
-    { "join"         , abcde  , {        } , "abcde"        ,       },
-    { "keypath"      , abc__  , {        } , "a.b.c"        ,       },
-    { "last"         , abc__  , {        } , "c"            ,       },
-    { "len"          , _____  , {        } , 0              ,       },
-    { "len"          , a_c__  , {        } , 2              ,       },
-    { "map"          , abc__  , { upper  } , ABC__          , false },
-    { "mul"          , abc__  , { 2      } , abcabc         , false },
-    { "mul"          , abc__  , { 0      } , _____          , false },
-    { "pop"          , abc__  , {        } , "c"            ,       },
-    { "pop"          , abcde  , { 1      } , "a"            ,       },
-    { "pop"          , abcde  , { 5      } , "e"            ,       },
-    { "prepend"      , _bc__  , { "a"    } , abc__          , true  },
-    { "reduce"       , _____  , { add, 5 } , 5              ,       },
-    { "reduce"       , n123   , { add    } , 6              ,       },
-    { "reduce"       , n123   , { add, 0 } , 6              ,       },
-    { "reduce"       , n123   , { add, 4 } , 10             ,       },
-    { "remove"       , abc__  , {        } , abc__          , true  },
-    { "remove"       , AZAZA  , { "A"    } , _ZAZA          , true  },
-    { "reverse"      , abcde  , {        } , edcba          , false },
-    { "slice"        , abcde  , {        } , abcde          , false },
-    { "slice"        , abcde  , { 2, 4   } , _bcd_          , false },
-    { "sort"         , edcba  , {        } , abcde          , true  },
-    { "take"         , abcde  , { 3      } , abc__          , false },
-    { "toset"        , AZAZA  , {        } , Set(AZAZA)     , false },
-    { "tostring"     , a___e  , {        } , '{ "a", "e" }' ,       },
-    { "uniq"         , AZAZA  , {        } , AZ___          , false },
-    { "zip"          , abc__  , { n12    } , { a1, b2 }     , false },
+    ------fname------|--list--|-----params-----|----expected----|-same_ref?---
+    { "all"          , _____  , { is_b       } , true           ,       },
+    { "all"          , abc__  , { is_b       } , false          ,       },
+    { "all"          , abc__  , { not_z      } , true           ,       },
+    { "any"          , abc__  , { is_b       } , true           ,       },
+    { "append"       , abc__  , {            } , abc__          , true  },
+    { "append"       , abc__  , { "d"        } , abcd_          , true  },
+    { "clear"        , abcde  , {            } , _____          , true  },
+    { "concat"       , abcde  , {            } , "abcde"        ,       },
+    { "contains"     , _bcd_  , {            } , false          ,       },
+    { "contains"     , _bcd_  , { "C"        } , false          ,       },
+    { "contains"     , _bcd_  , { "c"        } , true           ,       },
+    { "copy"         , abcde  , {            } , abcde          , false },
+    { "count"        , AZAZA  , {            } , 0              ,       },
+    { "count"        , AZAZA  , { 'A'        } , 3              ,       },
+    { "difference"   , a_c_e  , { _bcd_      } , a___e          , false },
+    { "difference"   , a_c_e  , { Set(_bcd_) } , a___e          , false },
+    { "drop"         , abc__  , { 1          } , _bc__          , false },
+    { "extend"       , a_c__  , { Set(____e) } , a_c_e          , true  },
+    { "extend"       , abc__  , { ___de      } , abcde          , true  },
+    { "extract"      , abc__  , { not_c      } , ab___          , false },
+    { "filter"       , _bcd_  , { not_c      } , _b_d_          , false },
+    { "first"        , abc__  , {            } , "a"            ,       },
+    { "flatten"      , {ac,e} , {            } , a_c_e          , false },
+    { "group_by"     , mixed  , { type       } , by_type        , false },
+    { "group_by"     , words  , { len        } , by_len         , false },
+    { "index_if"     , abcde  , { is_c       } , 3              ,       },
+    { "index"        , abcde  , {            } , nil            ,       },
+    { "index"        , abcde  , { "c"        } , 3              ,       },
+    { "insert"       , a_c__  , { 2, "b"     } , abc__          , true  },
+    { "insert"       , abc__  , { "d"        } , abcd_          , true  },
+    { "intersection" , a_c_e  , { _bcd_      } , __c__          , false },
+    { "intersection" , a_c_e  , { Set(_bcd_) } , __c__          , false },
+    { "invert"       , a___e  , {            } , a1_e2          , false },
+    { "join"         , abc__  , { ","        } , "a,b,c"        ,       },
+    { "join"         , abcde  , {            } , "abcde"        ,       },
+    { "keypath"      , abc__  , {            } , "a.b.c"        ,       },
+    { "last"         , abc__  , {            } , "c"            ,       },
+    { "le"           , _1_3_  , { _12__      } , false          ,       },
+    { "le"           , _12__  , { _1_3_      } , true           ,       },
+    { "le"           , _12__  , { _12__      } , true           ,       },
+    { "len"          , _____  , {            } , 0              ,       },
+    { "len"          , a_c__  , {            } , 2              ,       },
+    { "lt"           , _1_3_  , { _12__      } , false          ,       },
+    { "lt"           , _12__  , { __2__      } , true           ,       },
+    { "lt"           , _12__  , { _1_3_      } , true           ,       },
+    { "lt"           , _12__  , { _12__      } , false          ,       },
+    { "lt"           , _12__  , { _12_4      } , true           ,       },
+    { "map"          , abc__  , { upper      } , ABC__          , false },
+    { "mul"          , abc__  , { 0          } , _____          , false },
+    { "mul"          , abc__  , { 2          } , abcabc         , false },
+    { "pop"          , abc__  , {            } , "c"            ,       },
+    { "pop"          , abcde  , { 1          } , "a"            ,       },
+    { "pop"          , abcde  , { 5          } , "e"            ,       },
+    { "prepend"      , _bc__  , { "a"        } , abc__          , true  },
+    { "reduce"       , _____  , { add, 5     } , 5              ,       },
+    { "reduce"       , _123_  , { add        } , 6              ,       },
+    { "reduce"       , _123_  , { add, 0     } , 6              ,       },
+    { "reduce"       , _123_  , { add, 4     } , 10             ,       },
+    { "remove"       , abc__  , {            } , abc__          , true  },
+    { "remove"       , AZAZA  , { "A"        } , _ZAZA          , true  },
+    { "reverse"      , abcde  , {            } , edcba          , false },
+    { "slice"        , abcde  , {            } , abcde          , false },
+    { "slice"        , abcde  , { 2, 4       } , _bcd_          , false },
+    { "sort"         , edcba  , {            } , abcde          , true  },
+    { "take"         , abcde  , { 3          } , abc__          , false },
+    { "toset"        , AZAZA  , {            } , Set(AZAZA)     , false },
+    { "tostring"     , a___e  , {            } , '{ "a", "e" }' ,       },
+    { "uniq"         , AZAZA  , {            } , AZ___          , false },
+    { "zip"          , abc__  , { _12__      } , { a1, b2 }     , false },
+    { "zip"          , abc__  , { Set(_12__) } , { a1, b2 }     , false },
   }
   -- stylua: ignore end
 
@@ -112,7 +135,7 @@ describe("mods.List", function()
     local fname, ls, params, expected, same_ref = unpack(tests[i], 1, 5)
     ls = deepcopy(List(ls))
 
-    it(fmt("(%s):%s(%s) returns correct result", inspect(ls), fname, args_repr(params)), function()
+    it(fmt("List(%s):%s(%s) returns correct result", inspect(ls), fname, args_repr(params)), function()
       ---@diagnostic disable-next-line: param-type-mismatch
       local res = ls[fname](ls, unpack(params))
       assert.are_same(expected, res)
@@ -174,36 +197,10 @@ describe("mods.List", function()
       assert.are_equal('{ "a", "b", 1 }', tostring(List({ "a", "b", 1 })))
     end)
 
-    it("equals() compares lists with shallow equality", function()
-      assert.is_true(List({ "a", "b" }):equals(List({ "a", "b" })))
-      assert.is_true(List({ "a", "b", a1 }):equals(List({ "a", "b", a1 })))
-      assert.is_false(List({ "a", "b", {} }):equals(List({ "a", "b", {} })))
-      assert.is_false(List({ "a", "b" }):equals(List({ "a", "c" })))
-      assert.is_false(List({ "a", "b" }):equals(List({ "a", "b", "c" })))
-
-      local a = {}
-      assert.is_true(List({ a }):equals(List({ a })))
-      assert.is_false(List({ {} }):equals(List({ {} })))
-    end)
-
     it("__eq (==) compares lists with shallow equality", function()
       assert.is_true(List({ "a", "b" }) == List({ "a", "b" }))
       assert.is_false(List({ "a", "b" }) == List({ "a", "c" }))
       assert.is_false(List({ "a", "b" }) == List({ "a", "b", "c" }))
-    end)
-
-    it("lt() compares lists lexicographically", function()
-      assert.is_true(List({ 1, 2 }):lt({ 2 }))
-      assert.is_true(List({ 1, 2 }):lt({ 1, 3 }))
-      assert.is_true(List({ 1, 2 }):lt({ 1, 2, 0 }))
-      assert.is_false(List({ 1, 3 }):lt({ 1, 2 }))
-      assert.is_false(List({ 1, 2 }):lt({ 1, 2 }))
-    end)
-
-    it("le() compares lists lexicographically", function()
-      assert.is_true(List({ 1, 2 }):le({ 1, 2 }))
-      assert.is_true(List({ 1, 2 }):le({ 1, 3 }))
-      assert.is_false(List({ 1, 3 }):le({ 1, 2 }))
     end)
 
     it("__lt (<) compares lists lexicographically", function()
